289c5a73cc5b7d45d8e82ddf83957f49ff3875a3
DaeHee Han <gkseogml4959@gmail.com>
2024-09-02 20:10:39 +0900

    feat: 청소구역 포함 확인 조건 추가

--- src/core/rearrange.js
+++ src/core/rearrange.js
@@ -1,91 +1,3 @@
-// export const rearrange = (organization, absentPeople) => {
-//   const cleaningSystem = new CleaningSystem(organization);
-//   cleaningSystem.exclude(absentPeople);
-//   // cleaningSystem.arrangeRemainingPeople(4);
-//   // console.log(cleaningSystem.result);
-//   // cleaningSystem.arrangeRemainingPeople(3);
-//   // console.log(cleaningSystem.result);
-//   // cleaningSystem.arrangeRemainingPeople(2);
-//   // console.log(cleaningSystem.result);
-//   // cleaningSystem.arrangeRemainingPeople(1);
-//   // console.log(cleaningSystem.result);
-//   // return cleaningSystem.result;
-// };
-
-// class CleaningSystem {
-//   constructor(organization) {
-//     this.organization = organization;
-//   }
-
-//   // 휴가 혹은 근무 중인 인원을 청소 인원에서 제외한다.
-//   exclude(people) {
-//     console.log(people);
-//   }
-
-//   // id에 해당하는 청소구역의 남는 인원을 위쪽 청소구역에 배정한다.
-//   arrangeRemainingPeople(id) {
-//     const remainingPeople = this.getRemainingPeople(id);
-//     this.removeRemainingArea(id);
-//     this.arrange(id - 1, [...remainingPeople]);
-//   }
-
-//   // 현재 청소구역의 남는 인원을 구한다.
-//   getRemainingPeople(id) {
-//     const { cleaningAreaList } = this.organization.find((obj) => obj.id === id);
-//     console.log(cleaningAreaList);
-//     return cleaningAreaList
-//       .map((obj) => {
-//         const result = [];
-//         while (obj.assignedPeople.length > obj.maximum) {
-//           result.push(obj.assignedPeople.shift());
-//         }
-//         return result;
-//       })
-//       .flat();
-//   }
-
-//   removeRemainingArea(id) {
-//     const obj = this.organization.find((obj) => obj.id === id);
-//     obj.cleaningAreaList.forEach((cleaningArea) => {
-//       while (cleaningArea.assignedPeople.length > cleaningArea.maximum) {
-//         cleaningArea.assignedPeople.pop();
-//       }
-//     });
-
-//     this.organization = [...this.organization.filter((obj) => obj.id !== id), obj];
-//   }
-
-//   arrange(id, remainingPeople) {
-//     const obj = this.organization.find((obj) => obj.id === id);
-//     // 최대 인원이 아닌 청소구역에 배치
-//     obj.cleaningAreaList.forEach((cleaningArea) => {
-//       if (remainingPeople.length > 0 && cleaningArea.assignedPeople.length < cleaningArea.maximum) {
-//         const person = remainingPeople.shift();
-//         cleaningArea.assignedPeople.push(person);
-//       }
-//     });
-//     // 짬순 높은 사람이 있는 곳에 인원 배치
-//     const generationAndAreaNameObj = obj.cleaningAreaList
-//       .map((area) => area.assignedPeople.map((person) => ({ generation: person.generation, areaName: area.name })))
-//       .flat();
-//     generationAndAreaNameObj.sort((a, b) => a.generation - b.generation);
-
-//     while (remainingPeople.length > 0) {
-//       const person = remainingPeople.shift();
-//       const { areaName } = generationAndAreaNameObj.shift();
-//       generationAndAreaNameObj.push({ areaName });
-//       const area = obj.cleaningAreaList.find((area) => area.name === areaName);
-//       area.assignedPeople.push(person);
-//     }
-
-//     this.organization = [...this.organization.filter((obj) => obj.id !== id), obj];
-//   }
-
-//   get result() {
-//     return this.organization;
-//   }
-// }
-
 export const rearrange = (organization, absentPeople = [], remainingPeople = []) => {
   const cleaningSystem = new CleaningSystem(organization);
   cleaningSystem.exclude(absentPeople);
@@ -135,6 +47,7 @@ class CleaningSystem {
     }
   }
 
+  // 청소구역 중 가장 짬이 높은 사람 고르기
   getHighestPerson(cleaningAreaList) {
     const people = cleaningAreaList.map((area) => area.assignedPeople.map((person) => person)).flat();
     return people.reduce(
@@ -148,6 +61,7 @@ class CleaningSystem {
     );
   }
 
+  // 해당 인원이 포함된 청소구역 고르기
   getCleaningAreaByPerson(cleaningAreaList, person) {
     const result = cleaningAreaList.find((area) =>
       area.assignedPeople.some((p) => p.name === person.name && p.generation === person.generation),
@@ -162,7 +76,11 @@ class CleaningSystem {
     const maxAreaLength = obj.cleaningAreaList.length;
     for (let i = 0; i < maxAreaLength; i++) {
       isChanged = false;
-      if (obj.cleaningAreaList[i].maximum > obj.cleaningAreaList[i].assignedPeople.length) {
+      if (
+        obj.cleaningAreaList[i].maximum > obj.cleaningAreaList[i].assignedPeople.length &&
+        this.remainingPeople.length > 0 &&
+        obj.cleaningAreaList[i].isEnter(this.remainingPeople[0])
+      ) {
         obj.cleaningAreaList[i].assignedPeople.push(this.remainingPeople.shift());
         isChanged = true;
       }
@@ -174,16 +92,18 @@ class CleaningSystem {
       isChanged = false;
       const highestPerson = this.getHighestPerson(obj.cleaningAreaList);
       const { name } = this.getCleaningAreaByPerson(obj.cleaningAreaList, highestPerson);
-      const cleaingArea = obj.cleaningAreaList.find((area) => area.name === name);
+      const cleaningArea = obj.cleaningAreaList.find((area) => area.name === name);
       for (let i = 0; i < this.remainingPeople.length; i++) {
-        if (highestPerson.generation < this.remainingPeople[i].generation) {
-          console.log(highestPerson, cleaingArea, this.remainingPeople[i]);
+        if (
+          highestPerson.generation < this.remainingPeople[i].generation &&
+          cleaningArea.isEnter(this.remainingPeople[i])
+        ) {
           const temp = { ...this.remainingPeople[i] };
           this.remainingPeople[i] = { ...highestPerson };
-          cleaingArea.assignedPeople = cleaingArea.assignedPeople.filter(
+          cleaningArea.assignedPeople = cleaningArea.assignedPeople.filter(
             (p) => !(p.name === highestPerson.name && p.generation === highestPerson.generation),
           );
-          cleaingArea.assignedPeople.push(temp);
+          cleaningArea.assignedPeople.push(temp);
           isChanged = true;
           break;
         }
