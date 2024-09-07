6dd22c364a7e62adf24642ae46e31effe01bb64b
DaeHee Han <gkseogml4959@gmail.com>
2024-09-07 20:53:19 +0900

    feat: 최대 인원 대비 인원 추가 구현

--- src/core/rearrange.js
+++ src/core/rearrange.js
@@ -1,16 +1,17 @@
-export const rearrange = (organization, absentPeople = [], remainingPeople = []) => {
+export const rearrange = (organization, absentPeople = []) => {
   const cleaningSystem = new CleaningSystem(organization);
   cleaningSystem.exclude(absentPeople);
-  cleaningSystem.add(remainingPeople);
   cleaningSystem.start();
-  console.log(cleaningSystem.result);
   return cleaningSystem.result;
 };
 
 class CleaningSystem {
   constructor(organization) {
-    this.organization = organization;
-    this.remainingPeople = [];
+    this.remainingPeople = organization
+      .find((obj) => obj.id === 4)
+      .cleaningAreaList.map((area) => area.assignedPeople)
+      .flat();
+    this.organization = organization.filter((obj) => obj.id !== 4);
   }
 
   // 휴가 혹은 근무 중인 인원을 청소 인원에서 제외한다.
@@ -33,17 +34,28 @@ class CleaningSystem {
   }
 
   start() {
-    for (let i = 3; i > 0; i--) {
+    for (let i = 3; i > 1; i--) {
       this.arrange(i);
+    }
+
+    // 생활관 인원 배치 구현
+    const container = this.organization.find((obj) => obj.id === 1);
+    const maxLength = this.remainingPeople.length;
+    for (let remainLength = 0; remainLength < maxLength; remainLength++) {
+      const cmp = { ...this.remainingPeople[remainLength] };
+      for (let i = 0; i < container.cleaningAreaList.length; i++) {
+        const curp = container.cleaningAreaList[i].assignedPeople[0];
+        if (container.cleaningAreaList[i].isEnter(cmp) && curp.generation < cmp.generation) {
+          this.remainingPeople[0] = { ...curp };
+          container.cleaningAreaList[i].assignedPeople.shift();
+          container.cleaningAreaList[i].assignedPeople.push(cmp);
+        }
+      }
     }
 
     const obj = this.organization.find((obj) => obj.id === 0);
     while (this.remainingPeople.length > 0) {
       obj.cleaningAreaList[0].assignedPeople.push(this.remainingPeople.shift());
-    }
-
-    for (let i = 3; i > 0; i--) {
-      this.arrange(i);
     }
   }
 
@@ -71,23 +83,47 @@ class CleaningSystem {
 
   arrange(id) {
     const obj = this.organization.find((obj) => obj.id === id);
-    // 청소구역 중 최대인원이 아닌 곳에 remainingPeople 배치
-    let isChanged = true;
-    const maxAreaLength = obj.cleaningAreaList.length;
-    for (let i = 0; i < maxAreaLength; i++) {
-      isChanged = false;
-      if (
-        obj.cleaningAreaList[i].maximum > obj.cleaningAreaList[i].assignedPeople.length &&
-        this.remainingPeople.length > 0 &&
-        obj.cleaningAreaList[i].isEnter(this.remainingPeople[0])
-      ) {
-        obj.cleaningAreaList[i].assignedPeople.push(this.remainingPeople.shift());
-        isChanged = true;
-      }
+    let { idx: areaIdx } = obj.cleaningAreaList.reduce(
+      (pre, cur, i) => {
+        const gap = cur.maximum - cur.assignedPeople.length;
+        if (pre.gap < gap) {
+          return { gap, idx: i };
+        } else {
+          return pre;
+        }
+      },
+      { gap: 0, idx: -1 },
+    );
+
+    while (areaIdx !== -1 && this.remainingPeople.length > 0) {
+      obj.cleaningAreaList[areaIdx].assignedPeople.push(this.remainingPeople.shift());
+
+      areaIdx = obj.cleaningAreaList.reduce(
+        (pre, cur, i) => {
+          const gap = cur.maximum - cur.assignedPeople.length;
+          if (pre.gap < gap) {
+            return { gap, idx: i };
+          } else {
+            return pre;
+          }
+        },
+        { gap: 0, idx: -1 },
+      ).idx;
     }
 
+    // const maxAreaLength = obj.cleaningAreaList.length;
+    // for (let i = 0; i < maxAreaLength; i++) {
+    //   if (
+    //     obj.cleaningAreaList[i].maximum > obj.cleaningAreaList[i].assignedPeople.length &&
+    //     this.remainingPeople.length > 0 &&
+    //     obj.cleaningAreaList[i].isEnter(this.remainingPeople[0])
+    //   ) {
+    //     obj.cleaningAreaList[i].assignedPeople.push(this.remainingPeople.shift());
+    //   }
+    // }
+
     // 짬순으로 한명씩 검사한 이후 remainingPeople에 있는 인원이 들어갈 수 있고, 짬이 낮으면 바꿈
-    isChanged = true;
+    let isChanged = true;
     while (isChanged) {
       isChanged = false;
       const highestPerson = this.getHighestPerson(obj.cleaningAreaList);
