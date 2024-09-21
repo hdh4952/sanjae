b0cf9d84d2b424096837cf9bb74c28020d1e8d33
DaeHee Han <gkseogml4959@gmail.com>
2024-09-21 19:29:02 +0900

    design: css 추가

--- src/components/PersonCard.jsx
+++ src/components/PersonCard.jsx
@@ -1,6 +1,17 @@
 const PersonCard = ({ person }) => {
+  const { state } = person;
+  const bgColor = (() => {
+    if (state === 'off') {
+      return 'skyblue';
+    }
+    if (state === 'work') {
+      return 'tomato';
+    }
+    return '';
+  })();
+
   return (
-    <div>
+    <div style={{ backgroundColor: bgColor, marginBottom: '4px', padding: '4px' }}>
       {person.generation}기 {person.name}
     </div>
   );
--- src/components/TeamCard.jsx
+++ src/components/TeamCard.jsx
@@ -1,8 +1,33 @@
+import { useState } from 'react';
 import PersonCard from './PersonCard';
+import Modal from './Modal';
+import PersonStateCard from './PersonStateCard';
 
 const TeamCard = ({ team }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+    document.body.style.overflow = 'hidden';
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    document.body.style.overflow = 'auto';
+  };
+
   return (
     <>
+      {isModalOpen && (
+        <Modal closeModal={closeModal}>
+          <h3>
+            <span>{team.name}</span>
+          </h3>
+          {team.people.map((person) => (
+            <PersonStateCard key={person.generation + person.name} person={person} />
+          ))}
+        </Modal>
+      )}
       <div
         style={{
           width: '30vw',
@@ -15,7 +40,7 @@ const TeamCard = ({ team }) => {
           boxShadow: '0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23)',
         }}
         onClick={() => {
-          console.log('clicked');
+          openModal();
         }}
       >
         <div style={{ fontWeight: 'bold', fontSize: 'medium', marginBottom: '8px', borderRadius: '9999px' }}>
